151a2fafe7cb2ebf49540b02fa5c3b2e949ab533
Livia Ferreira Dos Santos <liferrei@c2r11p2.42sp.org.br>
2025-07-15 17:53:00 -0300

    livia

---
+++ CommonCore/Libft/ft_strncmp.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 17:04:57 by liferrei          #+#    #+#             */
+/*   Updated: 2025/07/15 17:50:48 by liferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t len)
+{
+	size_t			i;
+	unsigned char	*c1;
+	unsigned char	*c2;
+
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
+	i = 0;
+	if (len == 0)
+	{
+		return (0);
+	}
+	while (i < len)
+	{
+		if (c1[i] != c2[i] || c1[i] == '\0' || c2[i] == '\0')
+		{
+			return (c1[i] - c2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	const char str1[] = "livia";
+	const char str2[] = "lividih";
+	int result = 0;
+
+	result = ft_strncmp(str1, str2, 3);
+	if (result == 0)
+	{
+		printf("Even the size passed to the string is equal");
+	}
+	else
+	{
+		printf("Even the size passed to the string is not equal");
+	}
+}
+*/
--- CommonCore/Libft/libft.h
+++ CommonCore/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 12:27:59 by liferrei          #+#    #+#             */
-/*   Updated: 2025/07/15 16:17:04 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/07/15 17:07:18 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t len);
 
 #endif
