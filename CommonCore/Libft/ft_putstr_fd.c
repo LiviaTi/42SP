d7463d003226bd0c9254a43b57cd12fc6f49a7e4
Livia Ferreira Dos Santos <liferrei@student.42sp.org.br>
2025-07-23 20:13:39 -0300

    ft_putstr_fd.c

--- CommonCore/Libft/ft_putstr_fd.c
+++ CommonCore/Libft/ft_putstr_fd.c
@@ -6,11 +6,12 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 14:56:19 by liferrei          #+#    #+#             */
-/*   Updated: 2025/07/23 20:03:59 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/07/23 20:12:13 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 
 void	ft_putstr_fd(char *s, int fd)
 {
@@ -19,10 +20,10 @@ void	ft_putstr_fd(char *s, int fd)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		ft_putchar_fd (s[i], fd);
+		write(fd, &s[i], 1);
 		i++;
 	}
-	ft_putchar_fd ('\n',fd);
+	write(fd,"\n", 1);
 }
 
 #include <stdio.h>
@@ -30,7 +31,7 @@ void	ft_putstr_fd(char *s, int fd)
 int main(void)
 {
 	char str1[] = "Inteligent Livia";
-	int fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	int fd = open("test1.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 	{
 		printf("Error open the file");
@@ -38,4 +39,5 @@ int main(void)
 	}
 	ft_putstr_fd(str1, fd); 
 	close(fd);
+	return (0);
 }
