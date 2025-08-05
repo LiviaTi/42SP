24bdc3fb196638b1fcfa89ab4ab1d3ea3a43994c
Livia Ferreira Dos Santos <liferrei@student.42sp.org.br>
2025-08-05 15:26:40 -0300

    livia

--- CommonCore/1/ft_printf/ft_print_hex.c
+++ CommonCore/1/ft_printf/ft_print_hex.c
@@ -6,7 +6,26 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 17:32:18 by liferrei          #+#    #+#             */
-/*   Updated: 2025/07/30 17:48:20 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/08/05 15:26:18 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "printf.h"
+
+int	ft_print_hex(unsigned int num, int uppercase)
+{
+	char	*base;
+	char	c;
+	int		count;
+	 
+	count = 0;
+	if (uppercase == 1)
+		base = "0123456789abcdef";
+	if (uppercase == 2)
+		base = "0123456789ABCDEF";
+	if (num >= 16)
+		count += ft_print_hex(num / 16, uppercase);
+	c = base[num % 16];
+	count += write (1, &c, 1);
+	return (count);
+}
