1c4dec47d0a12a54fb88cb147d266e48c3c78414
Livia Ferreira Dos Santos <liferrei@student.42sp.org.br>
2025-08-08 10:30:00 -0300

    Livia

--- CommonCore/1/get_next_line/get_next_line_utils.c
+++ CommonCore/1/get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 12:07:57 by liferrei          #+#    #+#             */
-/*   Updated: 2025/08/07 16:48:42 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/08/08 10:28:34 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,58 @@ size_t	ft_strlen_gnl(char *s)
 	while (s[len] != '\0')
 		len++;
 	return (len);
+}
+
+char	*ft_strchr_gnl(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return((char *)s[i]);
+	}
+	if (s[i] == '\0')
+		return((char *)s[i]);
+	return(NULL);
+}
+
+char *ft_strjoin_gnl(const char *s1, const char *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	char	*s3;
+	
+	
+	s1_len = ft_strlen_gnl(s1);
+	s2_len = ft_strlen_gnl(s2);
+	s3 = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!s3)
+		return (NULL);
+	
+}
+
+char *ft_substr_gnl(const char *s)
+{
+	int		start;
+	int		end;
+	char 	*substr;
+	int 	i;
+
+	start = 0;
+	end = 0;
+	i = 0;
+	end = ft_strchr_gnl(s, '\n');
+
+	substr = malloc((end - start) * sizeof(char));
+	if (!substr)
+		return(NULL);
+	while (substr[i] != '\0' && i < end)
+	{
+		substr[i] = s[start];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
 }
