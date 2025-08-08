de60f0ecef7a8e668ba259d44b48dc79377c4108
Livia Ferreira Dos Santos <liferrei@student.42sp.org.br>
2025-08-08 13:20:39 -0300

    Livia

--- CommonCore/1/get_next_line/get_next_line_utils.c
+++ CommonCore/1/get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 12:07:57 by liferrei          #+#    #+#             */
-/*   Updated: 2025/08/08 10:28:34 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/08/08 13:20:23 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,43 +30,56 @@ char	*ft_strchr_gnl(const char *s, int c)
 	while (s[i] != '\0')
 	{
 		if (s[i] == (char)c)
-			return((char *)s[i]);
+			return ((char *)s[i]);
 	}
 	if (s[i] == '\0')
-		return((char *)s[i]);
-	return(NULL);
+		return ((char *)s[i]);
+	return (NULL);
 }
 
-char *ft_strjoin_gnl(const char *s1, const char *s2)
+char	*ft_strjoin_gnl(const char *s1, const char *s2)
 {
 	size_t	s1_len;
 	size_t	s2_len;
 	char	*s3;
-	
-	
+	int		i;
+	int		j;
+
 	s1_len = ft_strlen_gnl(s1);
 	s2_len = ft_strlen_gnl(s2);
+	i = 0;
+	j = 0;
 	s3 = malloc((s1_len + s2_len + 1) * sizeof(char));
 	if (!s3)
 		return (NULL);
-	
+	while (s1[i] != '\0')
+	{
+		s3[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		s3[i + j] = s2[j];
+		j++;
+	}
+	s3[i + j] = '\0';
+	return (s3);
 }
 
-char *ft_substr_gnl(const char *s)
+char	*ft_substr_gnl(const char *s)
 {
 	int		start;
 	int		end;
-	char 	*substr;
-	int 	i;
+	char	*substr;
+	int		i;
 
 	start = 0;
 	end = 0;
 	i = 0;
 	end = ft_strchr_gnl(s, '\n');
-
 	substr = malloc((end - start) * sizeof(char));
 	if (!substr)
-		return(NULL);
+		return (NULL);
 	while (substr[i] != '\0' && i < end)
 	{
 		substr[i] = s[start];
