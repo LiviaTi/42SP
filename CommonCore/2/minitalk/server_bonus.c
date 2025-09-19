4ae09d108f9f56156d22d3d760fd08878901d50a
Livia Ferreira Dos Santos <liferrei@student.42sp.org.br>
2025-09-19 18:12:06 -0300

    Livia

--- CommonCore/2/minitalk/server_bonus.c
+++ CommonCore/2/minitalk/server_bonus.c
@@ -6,36 +6,35 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 15:34:37 by liferrei          #+#    #+#             */
-/*   Updated: 2025/09/19 17:56:50 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/09/19 18:11:55 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
 
-#include "minitalk.h"
 
-static t_msg	g_msg;
+static t_server	g_server;
 
 static void	handle_signal(int sig, siginfo_t *info, void *context)
 {
 	(void)context;
-	if (g_msg.client_pid != info->si_pid)
+	if (g_server.client_pid != info->si_pid)
 	{
-		g_msg.client_pid = info->si_pid;
-		g_msg.bit_count = 0;
-		g_msg.current_char = 0;
+		g_sever.client_pid = info->si_pid;
+		g_sever.bit_count = 0;
+		g_sever.current_char = 0;
 	}
-	g_msg.current_char = (g_msg.current_char << 1) | (sig == SIGUSR2);
-	g_msg.bit_count++;
-	if (g_msg.bit_count == 8)
+	g_sever.current_char = (g_sever.current_char << 1) | (sig == SIGUSR2);
+	g_sever.bit_count++;
+	if (g_sever.bit_count == 8)
 	{
-		if (g_msg.current_char == '\0')
+		if (g_sever.current_char == '\0')
 			write(1, "\n", 1);
 		else
-			write(1, &g_msg.current_char, 1);
-		g_msg.bit_count = 0;
-		g_msg.current_char = 0;
-		kill(g_msg.client_pid, SIGUSR1);
+			write(1, &g_sever.current_char, 1);
+		g_sever.bit_count = 0;
+		g_sever.current_char = 0;
+		kill(g_sever.client_pid, SIGUSR1);
 	}
 }
 
@@ -44,9 +43,9 @@ int	main(void)
 	struct sigaction	sa;
 
 	ft_printf("Server PID: %d\n", getpid());
-	g_msg.client_pid = 0;
-	g_msg.bit_count = 0;
-	g_msg.current_char = 0;
+	g_sever.client_pid = 0;
+	g_sever.bit_count = 0;
+	g_sever.current_char = 0;
 	sa.sa_sigaction = handle_signal;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_SIGINFO;
